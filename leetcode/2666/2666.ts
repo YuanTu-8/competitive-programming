7e6578afe2a7da75a0245c5226812db7d479b39e
阿鈜 <tu.a.yuan.2022@gmail.com>
2026-02-04 21:56:22 +0800

    fix: 2666

--- leetcode/2666/2666.ts
+++ leetcode/2666/2666.ts
@@ -1,5 +1,5 @@
 type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
 
 function once(fn: Function): OnceFn {
     let called = false;
