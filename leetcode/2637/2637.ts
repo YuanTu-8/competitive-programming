27880b7985ae8d6138d16aba191328adc1356c19
阿鈜 <tu.a.yuan.2022@gmail.com>
2026-02-06 15:48:19 +0800

    fix: 2637

--- leetcode/2637/2637.ts
+++ leetcode/2637/2637.ts
@@ -1,7 +1,7 @@
 type Fn = (...params: any[]) => Promise<any>;
 
 function timeLimit(fn: Fn, t: number): Fn {
-    return async (...args) => {
+    return (...args) => {
         // Race between original function and timeout
         const timeout = new Promise<never>((_, reject) =>
             setTimeout(() => reject("Time Limit Exceeded"), t)
