49e0398d8601a600c6a6306176760473ced85eff
阿鈜 <tu.a.yuan.2022@gmail.com>
2026-02-24 21:20:53 +0800

    fix: 00136

--- uva/00136/main.cpp
+++ uva/00136/main.cpp
@@ -12,14 +12,16 @@ int main() {
     constexpr int primes[] = {2, 3, 5};
     int indices[] = {1, 1, 1};
 
-    auto candidate = [&](int i) { return ugly[indices[i]] * primes[i]; };
+    auto candidate = [&](int i) {
+        return ugly[indices[i]] * primes[i];
+    };
 
     for (int i = 2; i <= TARGET; ++i) {
         ugly[i] = std::min({candidate(0), candidate(1), candidate(2)});
 
-        for (int j = 0; j < 3; ++j) {
-            if (ugly[i] == candidate(j)) ++indices[j];
-        }
+        for (int j = 0; j < 3; ++j)
+            if (ugly[i] == candidate(j))
+                ++indices[j];
     }
 
     std::cout << "The 1500'th ugly number is " << ugly[TARGET] << ".\n";
