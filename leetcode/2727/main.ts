af47ef3d415bfc8a0d7d90c768e2e6dbe1c62967
阿鈜 <tu.a.yuan.2022@gmail.com>
2026-03-08 14:45:57 +0800

    fix: 2727

--- leetcode/2727/main.ts
+++ leetcode/2727/main.ts
@@ -2,8 +2,8 @@ type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string
 type Obj = Record<string, JSONValue> | JSONValue[];
 
 function isEmpty(obj: Obj): boolean {
-    for (const _ in obj) {
+    for (const _ in obj)
         return false;  // found at least one key
-    }
+
     return true;
 }
